177d795c98849fa178d590fa5652541fbd4251e6
Yevhenii Porodko <gporodko@gmail.com>
2025-05-16 11:28:01 +0300

    more githubuser info

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ const authConfig = {
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      profile(profile) {
+        console.log('profile(profile)', profile);
+        return {
+          id: profile.id,
+          name: profile.name || profile.login,
+          login: profile.login,
+          email: profile.email,
+        };
+      },
     }),
   ],
   secret: process.env.AUTH_SECRET,

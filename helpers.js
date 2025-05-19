65207d1419c539994f4855fb58192618c284aabf
Yevhenii Porodko <gporodko@gmail.com>
2025-05-19 14:17:26 +0300

    fix empty catch

--- helpers.js
+++ helpers.js
@@ -9,7 +9,7 @@ export async function getGamesArray() {
     const data = await fs.readFile(gamesFilePath, 'utf-8');
     const games = JSON.parse(data);
     return games;
-  } catch () {
+  } catch {
     return []; // TODO its not OK to return an empty array here, we should handle the error properly
   }
 }

484608672d29a0333229f4bd5c0198cbd6b983ec
Yevhenii Porodko <gporodko@gmail.com>
2025-05-19 12:48:46 +0300

    FE url in .env, skip error log

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ import { getGamesArray, validateGame, gamesFilePath } from './helpers.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON request body
 app.use(express.json());
-// Middleware to enable CORS
+
 app.use(
   cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
   })
 );
